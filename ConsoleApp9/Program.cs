250eaf7bf2c0089c8784aba9e120535d15aad9aa
Vira Kyzyma <vera.kyzyma8@gmail.com>
2024-02-06 11:59:13 +0200

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main()
+        {
+            string str = "hello";
+            for(int i=0; i<str.Length; i++)
+            {
+                int count = str.Length - str.Replace(str[i].ToString(), "").Length;
+                Console.WriteLine($"Symbol {str[i]}- {count}");
+                str = str.Replace(str[i].ToString(), "");
+                i--;
+            }
+        }
+    }
+}
